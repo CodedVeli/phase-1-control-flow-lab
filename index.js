7a56e805a3420d237581b987a5cb1e26d36d76da
eric githaiga <ericgithaiga007@gmail.com>
2023-09-18 15:23:25 +0300

    solution

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+
+
+  else if (distance > 2000 && distance <= 2500 ) {
+    return "I will gladly take your thirty bucks.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+  else if (distance > 2500) {
+
+    return "No can do.";
+  }
+  
+
 }
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(place){
   // Write your code here!
+  
+  return (place === "NYC") ? "Ok, sounds good." : "No go.";
+
+
+ 
+  }
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+
+
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+
+
+  // switch {
+  //   case "generous":
+  //     return "Thank you so much.";
+  //     break;
+  //   case "not as generous":
+  //     return "Thank you.";
+  // }
 }
